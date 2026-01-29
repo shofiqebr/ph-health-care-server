5e1db17587d1af3f22641c503aae0453a424443d
Md. shofiqul islam <shofiqebr@gmail.com>
2026-01-29 07:30:03 +0600

    add users

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,11 +5,10 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "prisma/schema/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
-  engine: "classic",
   datasource: {
     url: env("DATABASE_URL"),
   },
